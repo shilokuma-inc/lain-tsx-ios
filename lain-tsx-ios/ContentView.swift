9e5fa9ff0e0ce3bf189d35055a85f36761b0d0f1
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-02 04:49:31 +0900

    [chore] initial commit

---
+++ lain-tsx-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lain-tsx-ios
+//
+//  Created by 村石 拓海 on 2024/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lain-tsx-ios/lain_tsx_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lain_tsx_iosApp.swift
+//  lain-tsx-ios
+//
+//  Created by 村石 拓海 on 2024/05/02.
+//
+
+import SwiftUI
+
+@main
+struct lain_tsx_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
