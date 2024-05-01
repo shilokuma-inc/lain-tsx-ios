cc234fa0aee6508c96da177d59ea4df1074f38c2
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-02 05:13:27 +0900

    [feat] set webview upper

--- lain-tsx-ios/ContentView.swift
+++ lain-tsx-ios/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LainTSXWebView()
-            .frame(width: 800, height: 600)
+        VStack {
+            LainTSXWebView()
+                .frame(width: 800, height: 600)
+            
+            Spacer()
+        }
     }
 }
 
